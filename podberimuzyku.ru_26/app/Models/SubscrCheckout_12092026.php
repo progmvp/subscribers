15a839ab1b0b77c16c78d42a08974197503dbfdf
progmvp <149824647+progmvp@users.noreply.github.com>
2026-09-12 18:34:30 +0000

    Update SubscrCheckout_12092026.php

--- podberimuzyku.ru_26/app/Models/SubscrCheckout_12092026.php
+++ podberimuzyku.ru_26/app/Models/SubscrCheckout_12092026.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SubscrCheckout extends Model
+{
+    protected $table = 'subscr_checkout';
+
+    protected $connection = 'mysql';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'plan_id',
+        'payment_id',
+        'amount',
+        'status',
+        'expires_at',
+    ];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'expires_at' => 'datetime',
+    ];
+}
